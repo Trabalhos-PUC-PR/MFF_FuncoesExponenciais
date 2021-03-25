f50f260c1eafecec25367ffc9669549237fc36f0
Kovalski <81390604+Kovalski-rgb@users.noreply.github.com>
2021-03-25 19:01:51 -0300

    Update teste_circulo.py

--- teste_circulo.py
+++ teste_circulo.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-deltaNumero = np.arange(-200, 200, 0.1)
-raio = 1
+deltaNumero = np.arange(-200, 200, 0.1) #serve pra pegar o seno de todos os numeros entre -200 e 200 (incluindo os com uma casa decimal)
+raio = 1 #o raio é de um
 
-cos = raio * np.cos(deltaNumero)
-sen = raio * np.sin(deltaNumero)
+cos = raio * np.cos(deltaNumero) #isso calcula o cosseno
+sen = raio * np.sin(deltaNumero) #e isso calcula o seno
 
 plt.grid(color='lightgray', linestyle='-.', linewidth=0.1) #é basicamente uma copia do grid que ta na outra atividade
 
-plt.plot(cos, sen)
+plt.plot(cos, sen) #DESENHA O CIRCULO
 # mostar o gráfico
-plt.show()
+plt.show() #mostra o circulo
